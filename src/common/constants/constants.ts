b15286e5295c5e755a5d5430e7fa241e76dddaef
davidbzyae <102764748+davidbzyae@users.noreply.github.com>
2024-01-30 20:53:41 -0600

    feat: Add 'common' directory

---
+++ src/common/constants/constants.ts
@@ -0,0 +1,5 @@
+export const ASSETS_URL = "https://zyae.net/assets";
+export const BRAND_ASSETS_URL = `${ASSETS_URL}/images/brand/bluebird`;
+
+export const BLUEBIRD_LOGO_URL = `${ASSETS_URL}/images/brand/bluebird/bluebird.svg`;
+export const BLUEBIRD_FULL_LOGO_URL = `${BRAND_ASSETS_URL}/bluebirdFull.svg`;
