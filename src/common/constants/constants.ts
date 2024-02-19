610970bf2483a1ad4c0c9c23065d925223b5ccd3
David <102764748+davidbzyae@users.noreply.github.com>
2024-02-19 00:54:31 -0600

    chore: refactor for remote development

--- src/common/constants/constants.ts
+++ src/common/constants/constants.ts
@@ -1,5 +1,6 @@
-export const ASSETS_URL = "https://zyae.net/assets";
-export const BRAND_ASSETS_URL = `${ASSETS_URL}/images/brand/bluebird`;
+export const ASSETS_URL = `${document.location.href}/assets`;
 
-export const BLUEBIRD_LOGO_URL = `${ASSETS_URL}/images/brand/bluebird/bluebird.svg`;
-export const BLUEBIRD_FULL_LOGO_URL = `${BRAND_ASSETS_URL}/bluebirdFull.svg`;
+export const BLUEBIRD_LOGO_URL = `${ASSETS_URL}/bluebird.svg`;
+export const BLUEBIRD_FULL_LOGO_URL = `${ASSETS_URL}/bluebirdFull.svg`;
+
+export const PORTAL_APP_DEMO_IMAGE_URL = `${ASSETS_URL}/portal_app_demo_image.png`;
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "192.168.1.45",
+    port: 3000,
+    open: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
